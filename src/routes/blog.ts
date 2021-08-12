9d46f50b9575b21cb5328b1326ee2bea9220a52a
DefectingCat <i@defect.ink>
2021-08-12 20:48:06 +0800

    add query cache

--- src/routes/blog.ts
+++ src/routes/blog.ts
@@ -4,6 +4,7 @@ import superagent from 'superagent';
 import cheerio from 'cheerio';
 
 const blogURL = 'https://www.defectink.com';
+const aDay = 86400000;
 
 interface Post {
   title: string;
@@ -25,6 +26,37 @@ const getPost = (): Promise<string> => {
   });
 };
 
+/**
+ * 根据请求的首页 HTML
+ * 使用 cheerio 来获取 post 信息
+ * @returns
+ */
+const queryPost = async () => {
+  const res = await getPost();
+  const $ = cheerio.load(res);
+
+  const posts: Post[] = [];
+
+  $('.post-wrapper').each(function () {
+    const title = $(this).find('.article-title > a').text();
+    const content = $(this).find('.md').text();
+    const time = $(this).find('time').text();
+    const url = $(this).find('.readmore').attr('href');
+
+    posts.push({
+      title,
+      content,
+      time,
+      url,
+    });
+  });
+
+  return posts;
+};
+
+let cache: Post[]; // 缓存内容
+let cacheDate: number | null = null; // 缓存的时间
+
 const blog = async (
   fastify: FastifyInstance<Server, IncomingMessage, ServerResponse>
 ): Promise<void> => {
@@ -32,26 +64,20 @@ const blog = async (
    * 获取 blog 最新的 10 条 post
    */
   fastify.get('/', async () => {
-    const res = await getPost();
-    const $ = cheerio.load(res);
+    if (cacheDate == null) {
+      // 如果没有上次的查询时间，则直接更新缓存
+      cache = await queryPost();
+      cacheDate = Date.now();
+    } else {
+      // 如果间隔大于一天，则更新缓存
+      const isRenew = Date.now() - cacheDate > aDay;
+      if (isRenew) {
+        cache = await queryPost();
+        cacheDate = Date.now();
+      }
+    }
 
-    const posts: Post[] = [];
-
-    $('.post-wrapper').each(function () {
-      const title = $(this).find('.article-title > a').text();
-      const content = $(this).find('.md').text();
-      const time = $(this).find('time').text();
-      const url = $(this).find('.readmore').attr('href');
-
-      posts.push({
-        title,
-        content,
-        time,
-        url,
-      });
-    });
-
-    return posts;
+    return cache;
   });
 };
 
