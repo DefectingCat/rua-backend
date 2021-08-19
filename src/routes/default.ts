69710ab7f126c4946ebf1345a4648c0ef8e887a1
DefectingCat <i@defect.ink>
2021-08-20 00:01:44 +0800

    delete commet

--- src/routes/default.ts
+++ src/routes/default.ts
@@ -5,9 +5,6 @@ import logger from '../logger';
 const defaultRouter = async (
   fastify: FastifyInstance<Server, IncomingMessage, ServerResponse>
 ): Promise<void> => {
-  /**
-   * 获取 blog 最新的 6 条 post
-   */
   fastify.get('/', async (req: FastifyRequest) => {
     logger.info(req.headers); // 记录请求头
 
